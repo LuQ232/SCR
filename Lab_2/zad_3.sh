446d0ef37cb840ec70fb76125311a2e6e83cae99
LuQ232 <piotrbednarek99@gmail.com>
2020-10-14 21:03:33 +0200

    Zadanie 3

--- Lab_2/zad_3.sh
+++ Lab_2/zad_3.sh
@@ -3,9 +3,7 @@
 NAME=$1
 DATE_IN_DAYS=$2
 TAR_NAME=$3
-echo ${NAZWA}
-echo ${DATA}
-echo ${NAZWA_ARCHIWUM}
+
 
 find ~/Pulpit -name "$NAME" -mtime -"$DATE_IN_DAYS" | tar -cvf  "$TAR_NAME".tar.gz -T -
 exit
