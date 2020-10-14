a39077b8290ab28dfd65b217bf471b78fbfe4fcb
LuQ232 <piotrbednarek99@gmail.com>
2020-10-14 20:23:32 +0200

    Program zrobiony uniwersalnie LC_ALL=C

--- Lab_2/zad_1.sh
+++ Lab_2/zad_1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 export LC_ALL=C
-full_day_name=$(date +"%A") # %A zwraca pełną nazwę dnia
+FULL_DAY_NAME=$(date +"%A") # %A zwraca pełną nazwę dnia
 set `date`  #wywoluje komende date, kolejne parametry 'date' są pod $1,$2,$3 itd..
-DZIEN=$1 # pod zmienną dzień przypisuje pierwszy parametr komendy 'date'(czyli dzien)
-if [ ${DZIEN} = 'Sat' ] || [ ${DZIEN} = 'Sun' ]
+DAY=$1 # pod zmienną dzień przypisuje pierwszy parametr komendy 'date'(czyli dzien)
+if [ ${DAY} = 'Sat' ] || [ ${DAY} = 'Sun' ]
 then
-    echo "Today is $full_day_name. WEEKEND!"
+    echo "Today is $FULL_DAY_NAME. WEEKEND!"
 else
-    echo "Today is $full_day_name. WORKDAY!"
+    echo "Today is $FULL_DAY_NAME. WORKDAY!"
 fi
 
 
