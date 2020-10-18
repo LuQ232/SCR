4b73b23d113060824968c67e68ad315dd8f1ece3
LuQ232 <piotrbednarek99@gmail.com>
2020-10-18 19:05:12 +0200

    Drobna poprawka w robieniu tar'a

--- Lab_2/zad_3.sh
+++ Lab_2/zad_3.sh
@@ -5,7 +5,7 @@ DATE_IN_DAYS=$2
 TAR_NAME=$3
 
 
-find ~/Pulpit -name "$NAME" -mtime -"$DATE_IN_DAYS" | tar -cvf  "$TAR_NAME".tar.gz -T -
+find . -type f -mtime -"$DATE_IN_DAYS" -name "$NAME"  | xargs tar -czf  "$TAR_NAME".tar.gz 
 exit
 
 
