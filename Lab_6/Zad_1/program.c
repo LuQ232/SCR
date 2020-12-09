e34dc68bf1084a75d5b8496c0a1c407e370f3172
LuQ232 <piotrbednarek99@gmail.com>
2020-12-09 19:20:17 +0100

    Zad_1

---
+++ Lab_6/Zad_1/program.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+
+int main(){
+
+	char nazwa_pliku[64];
+	char obr_map[] = "./obr_map";
+	int plik_1;
+	int plik_2;
+	struct stat status_pliku;
+	void *addr = NULL;
+	
+	
+		while(1){
+			printf("Podaj nazwę pliku :\t");
+			scanf("%s",nazwa_pliku);
+			plik_1 = open(nazwa_pliku, O_RDONLY);
+			while(plik_1 < 0){
+				printf("Nie otwarto pliku\n");
+				printf("Podaj nową nazwę pliku:\t");
+				scanf("%s",nazwa_pliku);
+				plik_1 = open(nazwa_pliku, O_RDONLY);
+			}
+			printf("\n");
+			stat(nazwa_pliku, &status_pliku);
+			plik_2 = open(obr_map, O_RDWR);
+			truncate(obr_map,status_pliku.st_size);
+			addr = mmap(NULL, status_pliku.st_size,PROT_WRITE | PROT_READ, MAP_SHARED, plik_2, 0);
+			read(plik_1, addr, status_pliku.st_size);
+			msync(addr, status_pliku.st_size,MS_SYNC);
+			close(plik_1);
+			close(plik_2);
+		}
+
+	munmap(addr, status_pliku.st_size);
+	return 0;
+}
+
